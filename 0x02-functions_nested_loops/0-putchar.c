edb14e5426b97163b755f25b8d889f83a42c0377
root <connor0110john@gmail.com>
2023-01-30 19:16:56 -0800

    tasks

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,7 @@
-#include <stdio.h>/**
-* main - Prints the alphabet in lowercase.
-*
-* Return: Always 0.
-*
-*/
+#include <stdio.h>
+
 int main(void)
 {
-
-	putchar('_');
-	putchar('p');
-	putchar('u');
-	putchar('t');
-	putchar('c');
-	putchar('h');
-	putchar('a');
-	putchar('r');
-	putchar('\n');
-
-	return (0);
-}i
+	printf("putchar,\n);
+	return 0;
+} 
