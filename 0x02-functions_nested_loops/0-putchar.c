d0b60ab741d435502822b7fad056156fd525319b
root <connor0110john@gmail.com>
2023-01-30 19:27:40 -0800

    tasks

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
 
+/**
+ * main - Entrypoint
+ *
+ * Return: Always 0
+ */
 int main(void)
 {
-	printf("putchar,\n);
-	return 0;
-} 
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('a');
+	_putchar('\n');
+
+	return (0);
+	} 
