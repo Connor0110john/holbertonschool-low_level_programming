364f9fd7d7636d8248ae1c0e12c38737d8a60fbb
root <connor0110john@gmail.com>
2023-02-05 01:06:06 -0800

    task

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,20 @@
 #include <stdio.h>
 
+void print_alphabet(void)
+
 /**
  * main -check the code  
  *
  * Return: Always 0.
  */
-print_alphabet
+	int main(void) {
+		print_alphabet();
+		return 0;
+	}
+
+void print_alphabet(void)
 {
-		char c;
+	char c;
 
 	for (c = 'a' ; c <= 'z' ; c++)
 	{
