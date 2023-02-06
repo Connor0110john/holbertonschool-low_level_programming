e5fed4d53117bf815be4e73628c15c16f1a31bcd
root <connor0110john@gmail.com>
2023-02-05 20:15:48 -0800

    tasks

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,6 +1,19 @@
-#include <ctype.h>
+/*
+ * File: 0-isuperr.c
+ *
+ */
 
-int _isupper(int c) {
-	  return (c >= 'A' && c <= 'Z');
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * isupper - checks for upppercase characters
+ * return: 1 if character is uppercase, 0 otherwise.
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0)
 }
-
