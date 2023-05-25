daef219639049c1923677e6161e4a306717797e4
root <connor0110john@gmail.com>
2023-05-25 11:47:06 -0700

    commit

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -16,7 +16,7 @@ int main(void)
 	printf("Size of a char: %d byte(s)\n", sizeof(a));
 	printf("Size of a int: %d byte(s)\n", sizeof(b));
 	printf("Size of a long int: %d byte(s)\n", sizeof(c));
-	printf("Size of a long long int %d byte(s)", sizeof(d));
-	printf("Size of a float: 4 byte(s)", sizeof(e));
+	printf("Size of a long long int %d byte(s)\n", sizeof(d));
+	printf("Size of a float: 4 byte(s)\n", sizeof(e));
 	return (0);
 }
