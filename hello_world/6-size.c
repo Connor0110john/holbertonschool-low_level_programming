387fd8ab495d37218b85aab589903e9b2e887914
root <connor0110john@gmail.com>
2023-05-25 11:40:35 -0700

    commit

---
+++ hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - print the size fo various types on the computer it is comiled/run on
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
+
+	printf("Size of a char: %d byte(s)\n", sizeof(a));
+	printf("Size of a int: %d byte(s)\n", sizeof(b));
+	printf("Size of a long int: %d byte(s)\n", sizeof(c))
+	printf("Size of a long long int %d byte(s)", sizeof(d))
+	printf("Size of a float: 4 byte(s)", sizeof(e))
+	return (0);
+}
