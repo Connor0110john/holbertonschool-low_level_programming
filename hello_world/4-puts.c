3d89308d7efa42690173cfe35c8fb5af5a30741b
root <connor0110john@gmail.com>
2023-05-25 09:58:09 -0700

    commit

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
 /**
- * print 'Prigramming is like buiding a mutilingual puzzle' 
+ * main - print a text
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 			return (0);
-			}
+}
 
