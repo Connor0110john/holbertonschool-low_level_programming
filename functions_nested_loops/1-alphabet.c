fda47c573bc6a641a768327e00fb03c72d4c97dc
root <connor0110john@gmail.com>
2023-05-31 15:15:51 -0700

    commit

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-include "main.h"
 /**
  * print_alphabet - print the alphabet
  *
@@ -9,8 +8,8 @@ void print_alphabet(void)
 	char c;
 
 	for (c = 'a'; c <= 'z'; ++c)
-	putchar(c);
-	putchar('\n');
+	_putchar(c);
+	_putchar('\n');
 }
 
 /**
