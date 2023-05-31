1c22f25f68f05f33c59ab0823e68996cc6672fe5
root <connor0110john@gmail.com>
2023-05-31 14:15:10 -0700

    commit

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,23 +1,22 @@
 #include <stdio.h>
 
 /**
+ * main - check code
+ *
  * print_alphabet - print the alphabet
- * main - check the code
  *
  * Return: Always 0.
  */
-void print_alphabet(void);
-
 int main(void)
 {
+	void print_alphabet(void);
+
 	char c;
 
-	for (c = 'a'; c <= 'z'; ++c)
+	for (c = 'a'; c <= 'z'; c++)
+	{
 		putchar(c);
-
+	}
 	putchar('\n');
-
 	return (0);
 }
-
-void print_alphabet(void)
