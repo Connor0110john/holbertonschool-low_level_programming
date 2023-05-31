2d9939579d58cf14fe9f2310bc25aa92c65f6065
root <connor0110john@gmail.com>
2023-05-31 12:16:30 -0700

    commit

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/* use _putchar to print _putchar followed by a new line
+ * return 0;
+ */
+
+int main(){
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	return (0);
+}
