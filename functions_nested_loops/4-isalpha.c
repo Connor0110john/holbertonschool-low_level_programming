6706a031fa5b045b1c8594524a5901eac183c5e4
root <connor0110john@gmail.com>
2023-06-01 15:59:04 -0700

    commit

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,1 +1,17 @@
-#include <stdio.h>
+#include "main.h"
+
+/**
+ * _isalpha - check main
+ * @c: check
+ * Description: checks for alphabetic character
+ * Return: 1 if its a letter, 0 otherwise
+ */
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	return (1);
+
+	else
+
+	return (0);
+}
