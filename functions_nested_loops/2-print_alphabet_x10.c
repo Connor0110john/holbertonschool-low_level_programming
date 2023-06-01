c3b57dcfd3e37daada730bccc65f854a3cf493a4
root <connor0110john@gmail.com>
2023-06-01 15:04:27 -0700

    commit

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,13 +1,14 @@
 #include "main.h"
-/*
- * print_alphabet_x10 - prints the alphabet 10 times, followed by a new line.
- *
- * Return: (void)
+
+/**
+ * print_alphabet_x10 - checks main
+ * Description: function prints alphabet x10 times
+ * Return: void
  */
+
 void print_alphabet_x10(void)
 {
 	char c;
-
 	int i = 0;
 
 	while (i <= 9)
