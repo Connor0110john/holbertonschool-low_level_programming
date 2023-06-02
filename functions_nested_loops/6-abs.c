555d47583f931af906acedf06da1bf2a4f2f78f8
root <connor0110john@gmail.com>
2023-06-01 19:14:07 -0700

    commit

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -1,1 +1,13 @@
-#include <stdio.h>
+#include "main.h"
+
+/**
+ * _abs - check main
+ * @a: integer number.
+ * Return: absolute number.
+ */
+int _abs(int a)
+{
+	if (a < 0)
+	return (a * -1);
+	return (a);
+}
