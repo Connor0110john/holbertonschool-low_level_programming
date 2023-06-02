00440f8055ae3b070b8800ab4ca906ebf201e7ed
root <connor0110john@gmail.com>
2023-06-01 19:20:55 -0700

    commit

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,1 +1,14 @@
-#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_last_digit
+ * @n: interger number
+ * Return: last digit of n..
+ */
+{
+	int m;
+
+	m = abs(n % 10);
+	_putchar(m + '0');
+	return (m);
+}
