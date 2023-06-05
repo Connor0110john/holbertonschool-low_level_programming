32ff988647026a20533811452275db4a6a38cc68
root <connor0110john@gmail.com>
2023-06-05 11:58:38 -0700

    commit

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,1 +1,18 @@
 #include "main.h"
+
+/**
+ * print_most_numbers - print numbers from 0 to 9, dont print 2,4.
+ */
+
+void print_most_numbers(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		if (n != 2 && n != 4)
+			_putchar((n % 10) + '0');
+	}
+
+	_putchar('\n');
+}
