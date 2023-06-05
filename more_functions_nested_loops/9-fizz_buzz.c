46238990283f9baa0d233db0fd343dbd54686fc7
root <connor0110john@gmail.com>
2023-06-05 12:56:04 -0700

    commit

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -15,14 +15,14 @@ int main(void)
 		if ((n % 3) == 0 && (n % 5) == 0)
 			printf("FizzBuzz");
 
-		else if ((num % 3) == 0)
+		else if ((n % 3) == 0)
 			printf("Fizz");
 
-		else if ((num % 5) == 0)
+		else if ((n % 5) == 0)
 			printf("Buzz");
 
 		else
-			printf('%d', n);
+			printf("%d", n);
 
 		if (n == 100)
 			continue;
