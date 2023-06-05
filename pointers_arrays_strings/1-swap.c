6fd84eb009eb5263364c70696c38d9647f5f2db5
root <connor0110john@gmail.com>
2023-06-05 15:02:05 -0700

    commit

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -1,2 +1,14 @@
 #include "main.h"
 #include <stdio.h>
+
+/**
+ * swap_int - swaps values of two inegers
+ * @a: first integer to swap
+ * @b: second integer to swap
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = *b;
+	*b = *a;
+}
