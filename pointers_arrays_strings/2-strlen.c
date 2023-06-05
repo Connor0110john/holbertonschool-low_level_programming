d81f568e43d9a5152d4ce39ba45c2b086c3a2828
root <connor0110john@gmail.com>
2023-06-05 15:13:07 -0700

    commit

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,2 +1,16 @@
 #include "main.h"
 #include <stdio.h>
+
+/**
+ * _strlen - returns lenght of string.
+ * Return: @*s is lenght of string.
+ */
+int _strlen(char *s)
+{
+	size_t l = 0;
+
+	while (*s++)
+		l++;
+
+	return (l);
+}
