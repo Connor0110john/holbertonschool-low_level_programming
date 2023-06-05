9d8c13e2d223dc8dc350f82f997cc6d624fbb64a
root <connor0110john@gmail.com>
2023-06-05 12:28:13 -0700

    commit

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -1,1 +1,19 @@
 #include "main.h"
+
+/**
+ * print_diagonal - prints text diagonal
+ * @n: number of '\' to be printed
+ */
+
+void print_diagonal(int n)
+{
+	int line;
+
+	if (line > 0)
+	{
+		for (line = 0; line < n; line++)
+			_putchar('\');
+	}
+	
+	_putchar('\n');
+}
