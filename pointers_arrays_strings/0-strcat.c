8b0b054c73cdf11745738aacb699259f1caa3424
root <connor0110john@gmail.com>
2023-06-09 11:34:09 -0700

    commit

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * char *_strcat - a function that concatenates two strings
+ *
+ * @src: string to append dest to.
+ *
+ * @dest: string that src appends to.
+ *
+ * Return: dest
+ */
+
+char *_strcat(char *dest, char *src);
+{
+	size_t dest_len = strlen(dest);
+	size_t i;
+
+	for (i = 0 ; i < n && src[i] != '\0' ; i++)
+		dest[dest_len + i] = src[i];
+	dest[dest_len + i] = '\0';
+
+return (dest);
+}
