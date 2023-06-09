8fa022f9315df349d445427f4dd311cafeecc118
root <connor0110john@gmail.com>
2023-06-09 14:03:29 -0700

    commit

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,7 @@
+#include "main.h"
+#include <include>
+
+/**
+ * sting_toupper - changes all lowercase letters of a string to uppercase.
+ */
+char *string_toupper(char *)
---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,8 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * cap_sting - capitalizes all words of string.
+ */
+
+char *cap_string(char *)
