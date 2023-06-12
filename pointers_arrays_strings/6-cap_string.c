04a033c242de794845e6e0f7d5a34e2d1c50c9ba
root <connor0110john@gmail.com>
2023-06-12 13:08:39 -0700

    commit

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -2,7 +2,32 @@
 #include <stdio.h>
 
 /**
- * cap_sting - capitalizes all words of string.
+ * cap_string - capitalizes all words of string.
+ * @s: the string to be capitalized
+ *
+ * Return: a pointer to the changed string.
  */
 
-char *cap_string(char *)
+char *cap_string(char *s)
+{
+	int a = 0, i;
+	int scpc = 13;
+	char spc[] = {32, '\t', '\n', 44, ';', 46, '!', '?', '"', '(', ')', '{', '}'};
+
+	while (s[a])
+	{
+		i = 0;
+
+		while (i < cspc)
+		{
+			if ((a == 0 || s[a - 1] == spc[i]) && (s[a] >= 97 && s[a] <= 122))
+				s[a] -= 32;
+
+			i++;
+		}
+
+		a++;
+	}
+
+	return (s);
+}
