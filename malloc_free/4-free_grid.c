20b65ae5ac7cdad0befbed1d720e874e1e5bd9d6
root <connor0110john@gmail.com>
2023-07-01 21:28:08 -0700

    commit

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -3,4 +3,16 @@
 #include <stdlib.h>
 
 /**
+ * free_grid - freeing matrix
+ * @grid: matrix being passed
+ * @height: freeing using this variable
+ * Return: nothing
  */
+void free_grid(int **grid, int height)
+{
+	int a;
+
+	for (a = 0; a < height; a++)
+		free(grid[a]);
+	free(grid);
+}
