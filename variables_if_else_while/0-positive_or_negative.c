a2d5d9af6cd778d2abe44c2b10ff30c06e36f070
root <connor0110john@gmail.com>
2023-07-20 21:34:31 -0700

    task 0

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,13 +1,10 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/*
+/**
  * main - entry point
  * Description: Print if n is positive, negative or zero.
- * Return: Always 0 
- *
- *
- *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
