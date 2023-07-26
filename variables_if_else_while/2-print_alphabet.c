1db1f80f8db8d70d7af1738eeacc3a91ad2894a7
root <connor0110john@gmail.com>
2023-07-26 12:41:32 -0700

    fix

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/*
+ * main - entry point
+ * Description: prints the alphabet in lowercase, follwed by a new line.
+ * Return: Return 0
+ */
+
+int main (void)
+{
+	int alphabet
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		putchar(alphabet);
+	}
+	putchar("\n");
+	return (0);
+}
